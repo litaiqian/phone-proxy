095d23e4e5a070b85b3165f047f63728410b4399
litaiqian <litaiqian@sina.cn>
2026-05-28 03:10:14 +0800

    chore: remove old Python/Kivy files (replaced by native Android)

--- phone_proxy_app/core/__init__.py
+++
@@ -1,2 +0,0 @@
-# core/ 核心模块
-from .database import engine, SessionLocal, init_db, get_user_config, get_user_proxy
--- phone_proxy_app/slider/__init__.py
+++
@@ -1,2 +0,0 @@
-# slider/ 滑块验证模块
-from .slider_client import SliderClient, slider_client, check_services
